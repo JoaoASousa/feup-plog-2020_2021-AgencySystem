270ccb7946bc78c3c01a4715dc19b30df39ec510
Joao Sousa <joaosousa.pt@gmail.com>
2019-05-04 17:02:35 +0100

    menus & utilities files added

---
+++ Project2/Project2/menus.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+#include "menus.h"
+
+using namespace std;
+
+// IR ATUALIZANDO À MEDIDA QUE SE VAI ADICIONANDO FUNCIONALIDADES AO MENU
+vector<int> menuOptions = { 0, 1 };
+
+
+int mainMenu() {
+
+	bool menuFailFlags;
+	int operationSelector;
+
+	do {
+		menuFailFlags = false;
+
+		cout << "\nMain Menu\n" << endl;
+
+		cout << "  1. Agency Information" << endl;
+		cout << "  0. Quit" << endl;
+
+		cout << endl;
+
+		cout << "Please insert the corresponding number: ";
+		cin >> operationSelector;
+
+		if ((cin.fail()) || (count(menuOptions.begin(), menuOptions.end(), operationSelector) == 0)) {
+
+			if (cin.eof()) {
+				cout << "\nStopping the program . . ." << endl;
+				return -1;
+			}
+
+			menuFailFlags = true;
+			cin.clear();
+			cin.ignore(1000, '\n');
+
+			cout << "Invalid input" << endl;
+		}
+
+		cout << "\x1B[2J\x1B[H";
+		
+	} while (menuFailFlags);
+
+	return operationSelector;
+}
