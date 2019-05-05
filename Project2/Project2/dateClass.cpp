90512da503d2ef19ed20863061b30cfc90a32a2d
Joao Sousa <joaosousa.pt@gmail.com>
2019-05-05 12:45:17 +0100

    Started Implementing the Date Class

---
+++ Project2/Project2/dateClass.cpp
@@ -0,0 +1,72 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <ctime>
+
+#include "dateClass.h"
+
+using namespace std;
+
+
+// ------------------------------------------------------------------------
+// Constructors
+Date::Date() {
+
+	// current Date
+	time_t t = time(NULL);
+	tm* timePointer = localtime(&t);
+	
+	this->day = (timePointer->tm_mday);
+	this->month = (timePointer->tm_mon);
+	this->year = (timePointer->tm_year);
+
+}
+
+Date::Date(string date) {
+
+	// REQUIRES IMPLEMENTATION
+
+}
+
+
+Date::Date(unsigned short day, unsigned short month, unsigned year) {
+	this->day = day;
+	this->month = month;
+	this->year = year;
+}
+
+
+// ------------------------------------------------------------------------
+// Public Functions
+
+// set functions
+void Date::setDay(unsigned short day) {
+	this->day = day;
+}
+void Date::setMonth(unsigned short month) {
+	this->month = month;
+}
+
+void Date::setYear(unsigned year) {
+	this->year = year;
+}
+
+
+// get functions
+unsigned short Date::getDay() const {
+	return day;
+}
+
+unsigned short Date::getMonth() const {
+	return month;
+}
+
+unsigned Date::getYear() const {
+	return year;
+}
+
+
+ostream& operator<<(ostream& out, const Date & date) {
+
+	out << date.year << '/' << date.month << '/' << date.day;
+
+	return out;
+}
---
+++ Project2/Project2/dateClass.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+using namespace std;
+
+class Date {
+private:
+	unsigned short day;
+	unsigned short month;
+	unsigned year;
+
+public:
+	Date();
+	Date(string date);
+	Date(unsigned short day, unsigned short month, unsigned year);
+
+	// set functions
+	void setDay(unsigned short day);
+	void setMonth(unsigned short month);
+	void setYear(unsigned year);
+
+	// get functions
+	unsigned short getDay() const;
+	unsigned short getMonth() const;
+	unsigned getYear() const;
+
+
+	friend ostream& operator<<(ostream& out, const Date & date);
+
+};
